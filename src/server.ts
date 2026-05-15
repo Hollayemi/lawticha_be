c5e3a4d025c2f38cda6ecd467b5e68e2e64c2f72
Oluwasusi Stephen <stephanyemmitty@gmail.com>
2026-05-15 20:59:48 +0100

    worked on auth

--- src/server.ts
+++ src/server.ts
@@ -37,7 +37,7 @@ app.use('/api/', limiter);
 //  Core middleware 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin: process.env.CLIENT_URL || 'http://localhost:3000' ,
     credentials: true,
   })
 );
