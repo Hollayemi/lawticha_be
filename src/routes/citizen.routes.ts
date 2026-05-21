b7f3191df2936ea3958897d54c1fda64ca679a1e
Oluwasusi Stephen <stephanyemmitty@gmail.com>
2026-05-21 16:16:10 +0100

    update

--- src/routes/citizen.routes.ts
+++ src/routes/citizen.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
-import { UpdateCitizenProfileHandler } from '../controllers/citizen.controller';
+import { UpdateCitizenProfileHandler, } from '../controllers/citizen.controller';
 import { protect } from '../middleware/auth.middleware';
+import { submitVerificationHandler } from '../controllers/lawyer.controller';
 
 const router = Router();
 
 router.use(protect);
 
 // GET  /lawyer-profile
-// router.post('/lawyer-profile',  submitVerificationHandler);
+router.post('/lawyer-profile',  submitVerificationHandler);
 router.patch('/me/profile',  UpdateCitizenProfileHandler);
 
 
