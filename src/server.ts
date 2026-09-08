bf17bd392140f31ff343e9dc7caf78d879abcc17
Oluwasusi Stephen <stephanyemmitty@gmail.com>
2026-09-08 13:28:00 +0100

    --

--- src/server.ts
+++ src/server.ts
@@ -42,7 +42,7 @@ app.use(
     origin: [
       'https://www.lawticha.com',
       'https://lawticha.com',
-      'https://module.lawticha.com/',
+      'https://module.lawticha.com',
       'http://localhost:3000',
       'https://lawticha.vercel.app',
     ],
